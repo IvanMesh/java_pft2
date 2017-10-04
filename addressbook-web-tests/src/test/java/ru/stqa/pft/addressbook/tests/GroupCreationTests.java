2652d47e0f1c3b21b78dc9355c5258b7055c4326
IvanMesh <imeshcheriyakov@gmail.com>
2017-10-04 21:34:46 +0300

    Fluent в проверках + собственносозданный метод withModified

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,14 +1,12 @@
 package ru.stqa.pft.addressbook.tests;
 
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.*;
 
 
 public class GroupCreationTests extends TestBase {
@@ -18,16 +16,15 @@ public class GroupCreationTests extends TestBase {
   public void testGroopCreation() {
 
     app.goTO().GroupPage();
-    Set<GroupData> before = app.group().all();
+    Groups before = app.group().all();
     app.group().initGroupCreation();
     GroupData group = new GroupData().withName("test1");
     app.group().create(group);
-    Set<GroupData> after = app.group().all();
-    Assert.assertEquals(after.size(), before.size() + 1);
+    Groups after = app.group().all();
+    assertThat(after.size(),  equalTo(before.size() + 1));
 
-    group.withId(after.stream().mapToInt((g)->g.getId()).max().getAsInt());
-    before.add(group);
-    Assert.assertEquals(before, after);
+    assertThat(after, equalTo(
+            before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 
 }
